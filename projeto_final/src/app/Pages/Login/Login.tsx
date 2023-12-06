e92d91d62058a241662d71323f291ef88f96d9a3
Joao S4ntos <jpfsantos2004@gmail.com>
2023-12-06 18:08:22 -0300

    att

--- projeto_final/src/app/Pages/Login/Login.tsx
+++ projeto_final/src/app/Pages/Login/Login.tsx
@@ -1,8 +1,31 @@
-function Login(){
-    return(
-        <div></div>
+export default function LogIn () {
+     return( 
+        <main className="bg-[#F0EFEF]">
+            <div className="flex justify-center items-center min-w-screen h-screen">
+                <div className=" bg-white shadow-xl w-1/3 h-2/3 mb-16 mt-20 flex flex-col rounded-2xl border-0.1 border-black ">
+                    <div className="mb-16">
+                        <p className="text-black text-center text-2xl font-inter font-semibold mt-8"> Login </p>
+                    </div>
+
+                    <form className="justify-center mx-16" >
+                        <div className=" flex flex-col h-fit w-full  space-y-10 justify-center ">
+                            <input type="email" placeholder = 'EMAIL' className = 'border-0.1 border-black rounded-3xl h-10 text-center' />
+                            <input type="password" placeholder = 'SENHA' className='border-0.1 border-black rounded-3xl h-10 text-center' />
+                        </div>
+                        <div className="flex w-full h-fit justify-center items-center mt-10">
+                        <button type = "submit" className="bg-blue-900 w-1/2 h-12 rounded-3xl mt-10 ">
+                            <p className="text-white text-2xl">Entrar</p>
+                        </button>
+                        </div>
+                        <div className="flex w-full h-fit justify-center items-center mt-8"> 
+                            <a className="text-slate-500 hover:underline" href="./signin">Crie sua conta</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+            
+            
+        </main>
     )
-
 }
-
-export default Login;
