9b7e62bda1d6b58778b6454fb94c9690b6cf9d21
VOLP1 <64439930+VOLP1@users.noreply.github.com>
2023-12-08 20:24:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ const User = require('./User');
 
 async function main() {
     await mongoose.connect("mongodb://localhost:27017");
-    const user = await User.create({ name: "Kyle", age: 26 })
+    const user = await User.create({ 
+        title: 'Maçã Gala',
+        description: 'Maçãs frescas e doces, perfeitas para lanches e sobremesas.',
+        image: 'url_da_imagem_da_maca.jpg',
+        price: 3.50, // Supondo que o preço seja por quilo
+        previousPrice: 4.00,
+        isNew: false,
+        brand: 'Fazenda do Vale',
+        category: 'Frutas',
+        quantity: 200, // 200 kg disponíveis
+        carrinho: {
+            latidute: 1010.1,
+            Longetude: 21000,
+        },
+     })
     user.name = "Saally"
     console.log(user)
 }
