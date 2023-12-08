8006ae34c77b262776812e9c875d5061f0d3aef9
VOLP1 <64439930+VOLP1@users.noreply.github.com>
2023-12-08 20:24:20 -0300

    Update User.js

--- User.js
+++ User.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    age: Number
+const addressSchema = new mongoose.Schema({
+    Longetude: Number,
+    Latitude: Number,
 });
+
+const userSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  image: String,
+  price: Number,
+  previousPrice: Number,
+  isNew: Boolean,
+  brand: String,
+  category: String,
+  quantity: Number,
+  carrinho: addressSchema,
+}); 
 
 module.exports = mongoose.model('User', userSchema);
