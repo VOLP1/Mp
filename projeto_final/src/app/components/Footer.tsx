ff9017494c76904f48cd8ceae6a2f481b5764954
ogmoises <144768841+ogmoises@users.noreply.github.com>
2023-12-10 23:52:55 -0300

    Update Footer.tsx

--- projeto_final/src/app/components/Footer.tsx
+++ projeto_final/src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer(){
     return(
         <footer className="bg-green-700">
                 <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-                    <a href="#" className="text-xl font-bold text-black hover:text-gray-400">Feira Finder</a>
+                    <a href="/Homepage" className="text-xl font-bold text-black hover:text-gray-400">Feira Finder</a>
                     <p className="py-2 text-black sm:py-0">Trabalho MP-2023-2</p>
                 </div>
             </footer>
